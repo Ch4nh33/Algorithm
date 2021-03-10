819d245ede4fef7bdfcac7e3a6ad9dd0fb308916
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-03-11 07:58:43 +0900

    Create 1052 : [기초-비교연산] 두 정수 입력받아 비교하기4(설명) .c

---
+++ 1052 : [기초-비교연산] 두 정수 입력받아 비교하기4(설명) .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+    int num1 , num2;
+    scanf("%d %d",&num1,&num2);
+    
+    if (num1 != num2)
+    {
+        printf("%d",1);
+    }
+    else
+    {
+        printf("%d",0);
+    }
+    return 0;
+}
