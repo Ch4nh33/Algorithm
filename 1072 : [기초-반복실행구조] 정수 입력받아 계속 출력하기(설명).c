3eed168ecfc3f70058dda08a3df40a564ac10e4c
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-03-11 08:25:47 +0900

    Create 1072 : [기초-반복실행구조] 정수 입력받아 계속 출력하기(설명).c

---
+++ 1072 : [기초-반복실행구조] 정수 입력받아 계속 출력하기(설명).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+  int n, m;
+  scanf("%d", &n);
+  
+reget: 
+  scanf("%d", &m);
+
+  if(n-- != 0){ 
+    printf("%d\n", m);
+    goto reget;
+  }
+    return 0;
+}
