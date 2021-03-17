db477c52b44cad06bce79be1a757e45713ff0766
Ch4nh33 <78791504+Ch4nh33@users.noreply.github.com>
2021-03-18 04:01:54 +0900

    Create 1083 : [기초-종합] 3 6 9 게임의 왕이 되자!.c

---
+++ 1083 : [기초-종합] 3 6 9 게임의 왕이 되자!.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        
+        if (i%3==0||i%6==0||i%9==0)
+            printf("X ");
+        else
+            printf("%d ",i);
+    }
+    return 0;    
+}
